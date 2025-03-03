994cbdd790fe4072f6a176fb9d8f5a1c96fe8159
kulturhaus-bs <104384458+kulturhaus-bs@users.noreply.github.com>
2025-03-03 01:58:25 +0000

    website renewal

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,8 @@
 let blogJson = null;
 let currentBlogName;
+let currentBlogTitle;
+let currentBlogDescription;
+let currentBlogDate;
 let currentBlogImages;
 let currentImageIndex;
 
@@ -9,21 +12,25 @@ function onBlogClick(blogName) {
     const jsonData = blogJson[blogName]
     if (jsonData !== undefined) {
         currentBlogName = blogName;
+        currentBlogTitle = jsonData["title"];
+        currentBlogDescription = jsonData["description"];
+        currentBlogDate = jsonData["date"];
         currentBlogImages = jsonData["images"];
+
         changeBlogContent();
     }
 }
 
 function changeBlogContent() {
     currentImageIndex = 0;
-    const languageSelect = document.getElementById("language-select");
-    const language = languageSelect.value;
-    const currentBlogContent = translations["blogs"][currentBlogName];
+    //const languageSelect = document.getElementById("language-select");
+    //const language = languageSelect.value;
+    //const currentBlogContent = translations["blogs"][currentBlogName];
 
-    document.getElementById("blog-modal-type").textContent = currentBlogContent["type"][language];
-    document.getElementById("blog-modal-title").textContent = currentBlogContent["title"][language];
-    document.getElementById("blog-modal-description").textContent = currentBlogContent["description"][language];
-    document.getElementById("blog-modal-date").textContent = currentBlogContent["date"][language];
+    //document.getElementById("blog-modal-type").textContent = currentBlogContent["type"][language];
+    document.getElementById("blog-modal-title").textContent = currentBlogTitle;
+    document.getElementById("blog-modal-description").textContent = currentBlogDescription;
+    document.getElementById("blog-modal-date").textContent = currentBlogDate;
 
     showImage();
 }
@@ -48,7 +55,7 @@ function showNextBlogImage() {
 
 function showImage() {
     document.getElementById("blog-modal-image").src= currentBlogImages[currentImageIndex];
-    document.getElementById("image-number").textContent = (currentImageIndex + 1) + "/" + currentBlogImages.length;
+    document.getElementById("image-number").textContent = (currentImageIndex + 1) + " / " + currentBlogImages.length;
 }
 
 function onBlogClose() {
